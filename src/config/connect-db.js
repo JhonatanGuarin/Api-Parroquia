a8e18061368ab58e592bae0904d0a1d91beb695f
TatisLesmes <tatis26lesmesmail.com>
2024-07-11 16:41:51 -0500

    actualización user

--- src/config/connect-db.js
+++ src/config/connect-db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 
 
-//se debe quitar al subirlo al repositorio
-const URI = "mongodb+srv://jhonatancamilo99:12345@proyecto.j13wixj.mongodb.net/Parroquia_Santa_Maria?retryWrites=true&w=majority&appName=Proyecto";
 
-
-//const URI = process.env.DB_URI
+const URI = process.env.DB_URI
 
 
 mongoose.set('strictQuery')
